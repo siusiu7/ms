feffbe78fa53e8d747baef22f276b5afe568bf6c
siusiu7 <75193245+siusiu7@users.noreply.github.com>
2021-02-14 21:28:42 +0800

    Update qtt.js

--- qtt.js
+++ qtt.js
@@ -4,7 +4,7 @@ const cookieName = '趣头条'
 //const signXTKKey = 'senku_signXTK_qtt'
 //const readKey = 'senku_readKey_qtt'
 //const navCoinKey = 'senku_navCoinKey_qtt'
-
+const senku = init()
 const signVal = senku.getdata('process.env.SENKU_SIGNKEY_QTT')
 const signXTKVal = senku.getdata('process.env.SENKU_SIGNXTK_QTT')
 const readVal = senku.getdata('process.env.SENKU_READKEY_QTT')
@@ -835,4 +835,53 @@ function showmsg() {
 
   senku.msg(cookieName + ` 用户:${name}`, subTitle, detail)
   senku.done()
+}
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, resp, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, resp, resp.body))
+    }
+  }
+  return {
+    isSurge,
+    isQuanX,
+    msg,
+    log,
+    getdata,
+    setdata,
+    get,
+    post
+  }
 }
