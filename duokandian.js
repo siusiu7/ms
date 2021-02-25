cade57e146f4205f1170f08d7727513a5c4f005a
siusiu7 <75193245+siusiu7@users.noreply.github.com>
2021-02-25 15:49:39 +0800

    Update duokandian.js

--- duokandian.js
+++ duokandian.js
@@ -92,7 +92,7 @@ duokandianspdhheaderVal = {
 
 if ($.isNode()) {
     // 没有设置 DKD_duokandianCASH 则默认为 0 不提现
-    CASH = process.env.DKD_duokandianCASH || 0;
+    CASH = process.env.DKD_duokandianCASH || 1;
 }
 if ($.isNode() && process.env.DKD_duokandianBODY) {
     COOKIES_SPLIT = process.env.COOKIES_SPLIT || "\n";
