4e3e8e18a819686fd349da9b1e9330eaf21bfab8
siusiu7 <75193245+siusiu7@users.noreply.github.com>
2021-03-09 19:58:49 +0800

    Update sqxs.js

--- sqxs.js
+++ sqxs.js
@@ -164,7 +164,7 @@ function readbook() {
             try {
                 if (error) {
                     $.log("阅读请求失败,再次尝试阅读");
-                    await $.wait(1000);
+                    await $.wait(2000);
                     await readbook();
                 } else {
                     const result = JSON.parse(data)
@@ -172,13 +172,13 @@ function readbook() {
                         if (result.status == 200) {
                             ReadTimes++;
                             $.log("【阅读任务】第" + ReadTimes + "次阅读成功，获得3金币");
-                            await $.wait(1000);
+                            await $.wait(2000);
                             await readbook();
                         } else {
 
                             if (result.message != '领取达到每日上限，请明天再来') {
                                 $.log("【阅读任务】阅读失败，" + result.message + ",再次尝试阅读");
-                                await $.wait(1000);
+                                await $.wait(2000);
                                 await readbook();
                             } else
                                 $.log("【阅读任务】阅读失败，" + result.message);
